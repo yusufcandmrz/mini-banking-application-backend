07bfd808d722c9c8f798ba0f5db5a428cad0caa6
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-08-04 20:48:48 +0300

    update SecurityConfiguration

--- src/main/java/com/yusufcandmrz/minibank/config/SecurityConfiguration.java
+++ src/main/java/com/yusufcandmrz/minibank/config/SecurityConfiguration.java
@@ -48,15 +48,12 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
         configuration.setAllowedOrigins(List.of("http://localhost:8010"));
-        configuration.setAllowedMethods(List.of("GET","POST"));
-        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**",configuration);
-
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
